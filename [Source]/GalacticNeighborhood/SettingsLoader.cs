2a567d3e0d563135d63cf4122fb308066d9e061c
Sigma88 <sigmamodexpansions@outlook.com>
2017-06-07 00:26:53 +0200

    improvements on code

---
+++ [Source]/GalacticNeighborhood/SettingsLoader.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Kopernicus;
+using Kopernicus.Configuration;
+using System.Linq;
+using System;
+
+
+namespace GalacticNeighborhoodPlugin
+{
+    [ParserTargetExternal("Body", "ScaledVersion")]
+    public class SettingsLoader : BaseLoader
+    {
+        [ParserTarget("Coronas", optional = true, allowMerge = true)]
+        public CoronaFixer coronasFixer;
+
+        [ParserTarget("Light", optional = true, allowMerge = true)]
+        public ActiveFlareLoader activeFlareLoader;
+    }
+
+    public class ActiveFlareLoader : BaseLoader
+    {
+        [ParserTarget("activeSunFlare", optional = true)]
+        public AssetParser<Flare> activeFlare
+        {
+            set
+            {
+                if (value.value != null && !SunFlareSwitcher.activeFlares.ContainsKey(generatedBody.name))
+                    SunFlareSwitcher.activeFlares.Add(generatedBody.name, value.value);
+            }
+        }
+    }
+
+    public class CoronaFixer : BaseLoader
+    {
+        [ParserTarget("coronaTexture", optional = true, getAll = ",")]
+        public StringCollectionParser coronaTextures
+        {
+            set
+            {
+                SunCoronas[] coronas = generatedBody.scaledVersion?.GetComponentsInChildren<SunCoronas>(true);
+
+                for (int i = 0; i < Math.Min(coronas.Length, value.value.Count); i++)
+                {
+                    Material material = coronas[i]?.GetComponent<Renderer>()?.material;
+                    Texture mainTexture = Resources.FindObjectsOfTypeAll<Texture>().FirstOrDefault(t => t.name == value.value[i]);
+                    if (mainTexture != null && material != null)
+                        material.mainTexture = mainTexture;
+                }
+            }
+        }
+    }
+}
---
+++ [Source]/GalacticNeighborhood/SunFlareSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Kopernicus.Components;
+
+
+namespace GalacticNeighborhoodPlugin
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    class SunFlareSwitcher : MonoBehaviour
+    {
+        static KopernicusStar activeStar = null;
+        public static Dictionary<string, Flare> activeFlares = new Dictionary<string, Flare>();
+        static Flare oldFlare = null;
+
+        void Awake()
+        {
+            // Don't destroy SunFlareFixer if needed
+            if (activeFlares.Count > 0)
+                DontDestroyOnLoad(this);
+        }
+
+        void Update()
+        {
+            // When StarLightSwitcher changes the active star
+            if (KopernicusStar.Current != activeStar && activeFlares.ContainsKey(KopernicusStar.Current.name))
+            {
+                // Restore default sunFlare
+                if (activeStar != null && oldFlare != null)
+                    activeStar.sunFlare.flare = oldFlare;
+
+                // Restore default sunFlare
+                activeStar = KopernicusStar.Current;
+                if (activeStar == null || activeStar.sunFlare == null) return;
+                oldFlare = activeStar.sunFlare.flare;
+                activeStar.sunFlare.flare = activeFlares[activeStar.name];
+            }
+        }
+    }
+}
