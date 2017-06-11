ab337c17add57818d7af68025c9fbe3bd5135468
Sigma88 <sigmamodexpansions@outlook.com>
2017-06-11 02:49:40 +0200

    more coronas options

--- [Source]/GalacticNeighborhood/SettingsLoader.cs
+++ [Source]/GalacticNeighborhood/SettingsLoader.cs
@@ -3,6 +3,7 @@ using Kopernicus;
 using Kopernicus.Configuration;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 
 namespace GalacticNeighborhoodPlugin
@@ -10,11 +11,30 @@ namespace GalacticNeighborhoodPlugin
     [ParserTargetExternal("Body", "ScaledVersion")]
     public class SettingsLoader : BaseLoader
     {
-        [ParserTarget("Coronas", optional = true, allowMerge = true)]
-        public CoronaFixer coronasFixer;
-
         [ParserTarget("Light", optional = true, allowMerge = true)]
         public ActiveFlareLoader activeFlareLoader;
+
+        [ParserTargetCollection("GNCoronas", nameSignificance = NameSignificance.None)]
+        public List<GNCorona> GNcoronas
+        {
+            set
+            {
+                SunCoronas[] coronas = generatedBody.scaledVersion?.GetComponentsInChildren<SunCoronas>(true);
+
+                for (int i = 0; i < Math.Min(coronas.Length, value.Count); i++)
+                {
+                    Material material = coronas[i]?.GetComponent<Renderer>()?.material;
+                    Texture mainTexture = Resources.FindObjectsOfTypeAll<Texture>().FirstOrDefault(t => t.name == value[i].mainTexture);
+
+                    if (mainTexture != null && material != null)
+                        material.mainTexture = mainTexture;
+                    if (value[i].mainTextureOffset != null && material != null)
+                        material.mainTextureOffset = value[i].mainTextureOffset;
+                    if (value[i].mainTextureScale != null && material != null)
+                        material.mainTextureScale = value[i].mainTextureScale;
+                }
+            }
+        }
     }
 
     public class ActiveFlareLoader : BaseLoader
@@ -29,24 +49,20 @@ namespace GalacticNeighborhoodPlugin
             }
         }
     }
-
-    public class CoronaFixer : BaseLoader
+    
+    public class GNCorona
     {
-        [ParserTarget("coronaTexture", optional = true, getAll = ",")]
-        public StringCollectionParser coronaTextures
-        {
-            set
-            {
-                SunCoronas[] coronas = generatedBody.scaledVersion?.GetComponentsInChildren<SunCoronas>(true);
+        [ParserTarget("mainTexture", optional = true)]
+        public string mainTexture { get; set; }
 
-                for (int i = 0; i < Math.Min(coronas.Length, value.value.Count); i++)
-                {
-                    Material material = coronas[i]?.GetComponent<Renderer>()?.material;
-                    Texture mainTexture = Resources.FindObjectsOfTypeAll<Texture>().FirstOrDefault(t => t.name == value.value[i]);
-                    if (mainTexture != null && material != null)
-                        material.mainTexture = mainTexture;
-                }
-            }
+        [ParserTarget("mainTextureOffset", optional = true)]
+        public Vector2Parser mainTextureOffset { get; set; }
+
+        [ParserTarget("mainTextureScale", optional = true)]
+        public Vector2Parser mainTextureScale { get; set; }
+
+        public GNCorona()
+        {
         }
     }
 }
