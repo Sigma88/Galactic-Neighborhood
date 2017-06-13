66b1cb04d8643c1ccf7ef352999d63266e2d78cd
Sigma88 <sigmamodexpansions@outlook.com>
2017-06-13 02:23:09 +0200

    add procedural stars plugin

---
+++ [Source]/GalacticNeighborhood/ProceduralStars.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using Kopernicus;
+using Kopernicus.Configuration;
+using Kopernicus.Components;
+
+
+namespace GalacticNeighborhoodPlugin
+{
+    [ParserTargetExternal("Body", "ProceduralStar")]
+    public class ProceduralStar : BaseLoader, IParserEventSubscriber
+    {
+        [ParserTarget("temperature", optional = true)]
+        public NumericParser<int> temperature = 5772;
+
+        [ParserTarget("emitColor1", optional = true)]
+        public Texture2DParser emitColor1;
+
+        [ParserTarget("emitColor0", optional = true)]
+        public Texture2DParser emitColor0;
+
+        [ParserTarget("sunspotColor", optional = true)]
+        public Texture2DParser sunspotColor;
+
+        [ParserTarget("lightColor", optional = true)]
+        public Texture2DParser lightColors;
+
+        [ParserTarget("emitColorMult", optional = true)]
+        public NumericParser<float> emitColorMult = -1;
+
+        [ParserTarget("rimColorMult", optional = true)]
+        public NumericParser<float> rimColorMult = -1;
+
+        [ParserTarget("setOrbit", optional = true)]
+        public NumericParser<bool> setOrbit = true;
+
+        void IParserEventSubscriber.Apply(ConfigNode node)
+        {
+        }
+
+        void IParserEventSubscriber.PostApply(ConfigNode node)
+        {
+            // Apply changes to the Material
+            Material material = generatedBody.scaledVersion.GetComponent<Renderer>()?.sharedMaterial;
+
+            if (material != null)
+            {
+                if (emitColor0 != null)
+                {
+                    Color color = Pick(emitColor0);
+                    material.SetColor("_EmitColor0", color);
+
+                    if (emitColorMult > 0 && emitColorMult < float.MaxValue)
+                        material.SetColor("_EmitColor1", color * emitColorMult);
+
+                    if (rimColorMult > 0 && rimColorMult < float.MaxValue)
+                        material.SetColor("_RimColor", color * rimColorMult);
+                }
+
+                if (emitColor1 != null)
+                    material.SetColor("_EmitColor1", Pick(emitColor1));
+
+                if (sunspotColor != null)
+                    material.SetColor("_SunspotColor", Pick(sunspotColor));
+            }
+
+
+            // Apply changes to the LightShifter
+            LightShifter light = generatedBody.scaledVersion.GetComponentInChildren<LightShifter>();
+
+            if (light != null && lightColors != null)
+            {
+                Color color = Pick(lightColors);
+
+                light.ambientLightColor = color;
+                light.IVASunColor = color;
+                light.scaledSunlightColor = color;
+                light.sunlightColor = color;
+            }
+        }
+
+        Color Pick(Texture2D texture)
+        {
+            return texture.GetPixel((temperature - 2400) / 50, 0);
+        }
+    }
+}
