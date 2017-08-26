f42cf335ff4146544881361490d8309d510202e0
Sigma88 <sigmamodexpansions@outlook.com>
2017-08-26 13:08:20 +0200

    version check

---
+++ [Source]/GalacticNeighborhood/Version.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+namespace GalacticNeighborhoodPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class Version : MonoBehaviour
+    {
+        void Awake()
+        {
+            Debug.Log("Sigma Version Check:   Galactic Neighborhood v0.4.2");
+        }
+    }
+}
