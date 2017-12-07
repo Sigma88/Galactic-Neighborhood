3e5aca250529b66bf2a6dbda8f95cc11806a34d0
Sigma88 <sigmamodexpansions@outlook.com>
2017-12-07 01:33:59 +0100

    ambient light and bump version

---
+++ [Source]/GalacticNeighborhood/AmbientLight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace GalacticNeighborhoodPlugin
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class AmbientLightFix : MonoBehaviour
+    {
+        void Start()
+        {
+            if (GameSettings.AMBIENTLIGHT_BOOSTFACTOR == 0)
+                GameSettings.AMBIENTLIGHT_BOOSTFACTOR = -0.75f;
+        }
+    }
+}
