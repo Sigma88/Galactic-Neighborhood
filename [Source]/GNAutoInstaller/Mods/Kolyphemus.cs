364986cdea7d86e4c4c60f86a3148c4c22495d3c
Sigma88 <sigmamodexpansions@outlook.com>
2017-12-11 21:54:39 +0100

    add kolyphemus

---
+++ [Source]/GNAutoInstaller/Mods/Kolyphemus.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Kolyphemus : MonoBehaviour
+    {
+        static string archive;
+
+        void Awake()
+        {
+            Events.InstallMods.Add(Install);
+        }
+
+        void Install()
+        {
+            // Install GPP
+            archive = "PluginData/GalacticNeighborhood/Kolyphemus_System-Beta1.zip";
+
+            if (File.Exists(archive) && !Directory.Exists("GameData/Kolyphemus/"))
+            {
+                Archive.UnZip(archive, "Kolyphemus/", "GameData/Kolyphemus/");
+            }
+        }
+    }
+}
