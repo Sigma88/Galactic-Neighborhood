1e34149931315e4ef7e417859949b911f45635cc
Sigma88 <sigmamodexpansions@outlook.com>
2017-12-12 12:30:22 +0100

    kronkus asclepius

---
+++ [Source]/GNAutoInstaller/Mods/Asclepius.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Asclepius : MonoBehaviour
+    {
+        static string archive;
+
+        void Awake()
+        {
+            Events.InstallMods.Add(Install);
+        }
+
+        void Install()
+        {
+            // Install GPP
+            archive = "PluginData/GalacticNeighborhood/AsclepiusV3-0.zip";
+
+            if (File.Exists(archive) && !Directory.Exists("GameData/Asclepius/"))
+            {
+                Archive.UnZip(archive, "GameData/Asclepius/", "GameData/Asclepius/");
+            }
+        }
+    }
+}
---
+++ [Source]/GNAutoInstaller/Mods/Kronkus.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Kronkus : MonoBehaviour
+    {
+        static string archive;
+
+        void Awake()
+        {
+            Events.InstallMods.Add(Install);
+        }
+
+        void Install()
+        {
+            // Install GPP
+            archive = "PluginData/GalacticNeighborhood/KronkusV1-3-4.zip";
+
+            if (File.Exists(archive) && !Directory.Exists("GameData/Kronkus/"))
+            {
+                Archive.UnZip(archive, "GameData/Kronkus/", "GameData/Kronkus/");
+                Archive.UnZip(archive, "GameData/Spud/", "GameData/Spud/");
+            }
+        }
+    }
+}
