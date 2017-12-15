175bb16ff2c7f6278095ace2ae1024beaefd6dc1
Sigma88 <sigmamodexpansions@outlook.com>
2017-12-15 13:59:43 +0100

    check dependancies

--- [Source]/GNAutoInstaller/Mods/Almost.cs
+++ [Source]/GNAutoInstaller/Mods/Almost.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using UnityEngine;
 
 
@@ -8,7 +9,6 @@ namespace GNAutoInstallerPlugin
     public class Almost : MonoBehaviour
     {
         static string archive;
-        static string sigdim;
 
         void Awake()
         {
@@ -17,25 +17,21 @@ namespace GNAutoInstallerPlugin
 
         void Install()
         {
+            // Check Dependancies
+            if (AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.name == "SigmaDimensions") == null) return;
+
             // Install Almost
             archive = "PluginData/GalacticNeighborhood/Almost_Real_Solar_System-1.2.1.zip";
-            sigdim = "PluginData/GalacticNeighborhood/Sigma-Dimensions.v0.9.6.zip";
 
             if (File.Exists(archive) && !Directory.Exists("GameData/(Almost)RealSolarSystem/"))
             {
-                if (File.Exists(sigdim) || Directory.Exists("GameData/Sigma/Dimensions/"))
+                string[] filter = new string[]
                 {
-                    string[] filter = new string[]
-                    {
                         "GameData/(Almost)RealSolarSystem/Compatibility/KSCSwitcher/",
                         "GameData/(Almost)RealSolarSystem/Compatibility/RemoteTech/"
-                    };
+                };
 
-                    Archive.UnZip(archive, "GameData/(Almost)RealSolarSystem/", "GameData/(Almost)RealSolarSystem/", filter);
-
-                    if (!Directory.Exists("GameData/Sigma/Dimensions/"))
-                        Archive.UnZip(sigdim, "GameData/Sigma/Dimensions/", "GameData/Sigma/Dimensions/");
-                }
+                Archive.UnZip(archive, "GameData/(Almost)RealSolarSystem/", "GameData/(Almost)RealSolarSystem/", filter);
             }
         }
     }
