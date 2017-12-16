e183ded39506d1e1434c75f67c8b9b696fa64a3b
Sigma88 <sigmamodexpansions@outlook.com>
2017-12-16 01:14:29 +0100

    boris

---
+++ [Source]/GNAutoInstaller/Mods/Boris.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Boris : MonoBehaviour
+    {
+        static string archive;
+
+        void Awake()
+        {
+            Events.InstallMods.Add(Install);
+        }
+
+        void Install()
+        {
+            // Install Boris
+            archive = "PluginData/GalacticNeighborhood/BorisSystem.v0.5.zip";
+
+            if (File.Exists(archive) && !Directory.Exists("GameData/PFSystems/"))
+            {
+                Archive.UnZip(archive, "GameData/PFSystems/", "GameData/PFSystems/");
+            }
+        }
+    }
+}
