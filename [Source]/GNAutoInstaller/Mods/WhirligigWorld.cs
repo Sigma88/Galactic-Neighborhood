f142c8786b6d5b79a3ce4fb3173cde98756e3589
Sigma88 <sigmamodexpansions@outlook.com>
2017-12-21 16:41:01 +0100

    WhirligigWorld

---
+++ [Source]/GNAutoInstaller/Mods/WhirligigWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class WhirligigWorld : Pack<NewHorizons>
+    {
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/WhirligigWorld.zip"; } }
+        internal override string path { get { return "GameData/WhirligigWorld/"; } }
+        internal override string[] filter
+        {
+            get
+            {
+                return new string[]
+                {
+                    "WhirligigWorld/ModCompatibility/Clouds/",
+                    "WhirligigWorld/Patches/"
+                };
+            }
+        }
+
+        internal override void Install()
+        {
+            if (!Directory.Exists(path))
+            {
+                Archive.UnZip(archive, "WhirligigWorld/", path, filter);
+            }
+        }
+    }
+}
