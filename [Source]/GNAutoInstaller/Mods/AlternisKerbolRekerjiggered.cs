144263c59ecb942f85c10c50cb9c68c69cb537cd
Sigma88 <sigmamodexpansions@outlook.com>
2018-01-25 14:46:56 +0100

    fix jungis and AKR

--- [Source]/GNAutoInstaller/Mods/AlternisKerbolRekerjiggered.cs
+++ [Source]/GNAutoInstaller/Mods/AlternisKerbolRekerjiggered.cs
@@ -1,22 +1,30 @@
-﻿namespace GNAutoInstallerPlugin
+﻿using System.IO;
+
+
+namespace GNAutoInstallerPlugin
 {
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class AKR : Pack<AKR>
     {
-        internal override string archive { get { return "PluginData/GalacticNeighborhood/AlternisKerbolRekerjiggered2.2.0.zip"; } }
-        internal override string path { get { return "GameData/AlternisKerbolRekerjiggered/"; } }
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/AlternisKerbolRekerjiggered2.3.0.4.zip"; } }
+        internal override string path { get { return "AlternisKerbolRekerjiggered/"; } }
         internal override string[] filter
         {
             get
             {
                 return new string[]
                 {
-                    path + "ModCompatibility/EVE_Clouds/",
-                    path + "ModCompatibility/PlanetPackCompatibility",
-                    path + "ModCompatibility/PluginData",
-                    path + "ModCompatibility/Kronometer.cfg",
-                    path + "ModCompatibility/SLIPPIST-1.cfg"
+                    path + "ModCompatibility/"
                 };
+            }
+        }
+
+        internal override void Install()
+        {
+            if (!Directory.Exists("GameData/" + path))
+            {
+                Archive.UnZip(archive, path, "GameData/" + path, filter);
+                Archive.UnZip(archive, path + "ModCompatibility/PlanetPackCompatibility/GalacticNeighborhood.cfg", "GameData/" + path + "ModCompatibility/PlanetPackCompatibility/GalacticNeighborhood.cfg");
             }
         }
     }
--- [Source]/GNAutoInstaller/Mods/Jungis.cs
+++ [Source]/GNAutoInstaller/Mods/Jungis.cs
@@ -3,7 +3,7 @@
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class Jungis : Pack<Jungis>
     {
-        internal override string archive { get { return "PluginData/GalacticNeighborhood/Jungis Planet Pack.zip"; } }
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/Jungis+Planet+Pack.zip"; } }
         internal override string path { get { return "GameData/Jungis/"; } }
     }
 }
