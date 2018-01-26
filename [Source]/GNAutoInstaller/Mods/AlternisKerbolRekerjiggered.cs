a845eb76d2c4cc90070f532715603e49cb2712d6
Sigma88 <sigmamodexpansions@outlook.com>
2018-01-26 10:34:49 +0100

    fix akr

--- [Source]/GNAutoInstaller/Mods/AlternisKerbolRekerjiggered.cs
+++ [Source]/GNAutoInstaller/Mods/AlternisKerbolRekerjiggered.cs
@@ -7,24 +7,25 @@ namespace GNAutoInstallerPlugin
     class AKR : Pack<AKR>
     {
         internal override string archive { get { return "PluginData/GalacticNeighborhood/AlternisKerbolRekerjiggered2.3.0.4.zip"; } }
-        internal override string path { get { return "AlternisKerbolRekerjiggered/"; } }
+        internal override string path { get { return "GameData/AlternisKerbolRekerjiggered/"; } }
         internal override string[] filter
         {
             get
             {
                 return new string[]
                 {
-                    path + "ModCompatibility/"
+                    path + "ModCompatibility/",
+                    path + "KopernicusFiles/000"
                 };
             }
         }
 
         internal override void Install()
         {
-            if (!Directory.Exists("GameData/" + path))
+            if (!Directory.Exists(path))
             {
-                Archive.UnZip(archive, path, "GameData/" + path, filter);
-                Archive.UnZip(archive, path + "ModCompatibility/PlanetPackCompatibility/GalacticNeighborhood.cfg", "GameData/" + path + "ModCompatibility/PlanetPackCompatibility/GalacticNeighborhood.cfg");
+                Archive.UnZip(archive, "AlternisKerbolRekerjiggered/", path, filter);
+                Archive.UnZip(archive, "AlternisKerbolRekerjiggered/ModCompatibility/PlanetPackCompatibility/GalacticNeighborhood.cfg", path + "ModCompatibility/PlanetPackCompatibility/GalacticNeighborhood.cfg");
             }
         }
     }
