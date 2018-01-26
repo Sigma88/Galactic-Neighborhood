9240b0598f9488ee8497225b193a3bbb3064d4c6
Sigma88 <sigmamodexpansions@outlook.com>
2018-01-26 12:55:45 +0100

    add AK and BK

---
+++ [Source]/GNAutoInstaller/Mods/AfterKerbin.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class AfterKerbin : Pack<AfterKerbin>
+    {
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/After_Kerbin_Planet_Mod-1.0.3.zip"; } }
+        internal override string path { get { return "GameData/AfterKerbin/"; } }
+        internal override string[] filter
+        {
+            get
+            {
+                return new string[]
+                {
+                    "AfterKerbin/_Core00/KEX_EVAfootprints.cfg",
+                    "AfterKerbin/EVE/",
+                    "AfterKerbin/LSM/",
+                    "AfterKerbin/scatterer/",
+                    "AfterKerbin/LoadingScreenManager.dll"
+                };
+            }
+        }
+
+        internal override void Install()
+        {
+            if (!Directory.Exists(path))
+            {
+                Archive.UnZip(archive, "AfterKerbin/", path, filter);
+
+                CTTP.Install();
+            }
+        }
+    }
+}
