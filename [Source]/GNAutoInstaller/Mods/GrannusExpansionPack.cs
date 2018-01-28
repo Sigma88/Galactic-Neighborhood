da31f45cbe1609e30744c102eb9562f35697495d
Sigma88 <sigmamodexpansions@outlook.com>
2018-01-28 18:35:27 +0100

    fix GPE

---
+++ [Source]/GNAutoInstaller/Mods/GrannusExpansionPack.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+
+namespace GNAutoInstallerPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class GEP : Pack<GEP>
+    {
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/GrannusExpansionPack_0.8.0.0.zip"; } }
+        internal override string path { get { return "GameData/GEP/"; } }
+        internal override string[] filter
+        {
+            get
+            {
+                return new string[]
+                {
+                    path + "GPP_Configs",
+                    path + "GEP_Resources",
+                    path + "GEP_Scatterer",
+                    path + "MiniAVC.dll"
+                };
+            }
+        }
+
+        internal override bool Check()
+        {
+            if (!Directory.Exists(path))
+            {
+                return File.Exists(archive) && GPP.Mod.Check();
+            }
+
+            return true;
+        }
+    }
+}
