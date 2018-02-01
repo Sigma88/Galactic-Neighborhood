2eb1a9708948b60309e8ac9f6f09092e5b8b707b
Sigma88 <sigmamodexpansions@outlook.com>
2018-02-01 10:04:22 +0100

    bump gpo version

--- [Source]/GNAutoInstaller/Mods/GameslinxsPlanetOverhaul.cs
+++ [Source]/GNAutoInstaller/Mods/GameslinxsPlanetOverhaul.cs
@@ -6,7 +6,7 @@ namespace GNAutoInstallerPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class GPO : Pack<GPO>
     {
-        internal override string archive { get { return "PluginData/GalacticNeighborhood/Gameslinxs_Planet_Overhaul_Stockalike_Planet_Pack-3.0.1.zip"; } }
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/Gameslinxs_Planet_Overhaul_Stockalike_Planet_Pack-3.0.2.zip"; } }
         internal override string path { get { return "GameData/GPO/"; } }
         internal override string[] filter
         {
