f485713d2b46478832d86cccf13b9e32b4755a4c
Sigma88 <sigmamodexpansions@outlook.com>
2019-08-13 15:33:54 +0200

    asclepius and kronkus fix

--- [Source]/GNAutoInstaller/Mods/Kronkus.cs
+++ [Source]/GNAutoInstaller/Mods/Kronkus.cs
@@ -6,7 +6,7 @@ namespace GNAutoInstallerPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class Kronkus : Pack<Kronkus>
     {
-        internal override string archive { get { return "PluginData/GalacticNeighborhood/KronkusV1-3-4.zip"; } }
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/KronkusV1-5.zip"; } }
         internal override string path { get { return "GameData/Kronkus/"; } }
 
         internal override void Install()
