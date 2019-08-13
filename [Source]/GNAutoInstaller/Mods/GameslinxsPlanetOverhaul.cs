a8286ac1bec30a0c3b9878191ecd6c9c19f85efe
Sigma88 <sigmamodexpansions@outlook.com>
2019-08-13 18:41:44 +0200

    gpo fix

--- [Source]/GNAutoInstaller/Mods/GameslinxsPlanetOverhaul.cs
+++ [Source]/GNAutoInstaller/Mods/GameslinxsPlanetOverhaul.cs
@@ -6,7 +6,7 @@ namespace GNAutoInstallerPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class GPO : Pack<GPO>
     {
-        internal override string archive { get { return "PluginData/GalacticNeighborhood/Gameslinxs_Planet_Overhaul_Stockalike_Planet_Pack-3.0.2.zip"; } }
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/Gameslinx.s.Planet.Overhaul.3.4.zip"; } }
         internal override string path { get { return "GameData/GPO/"; } }
         internal override string[] filter
         {
@@ -16,12 +16,18 @@ namespace GNAutoInstallerPlugin
                 {
                     path + "Configs/FixPQS.cfg",
                     path + "EVE/",
-                    path + "LSM/",
+                    path + "LoadingScreens/",
                     path + "scatterer/",
+                    path + "ShoutAtPeopleWhoCantInstallModsProperly/",
+                    path + "Skybox/",
                     path + "GPO_RemoteTech.cfg",
                     path + "GPO_Scatterer.cfg",
                     path + "GPO_Scatterer_Settings.cfg",
-                    path + "GPO_Science Balance Pass.cfg"
+                    path + "GPO_ScattererInitialiser.cfg",
+                    path + "GPO_Science Balance Pass.cfg",
+                    path + "GPO_Skybox.cfg",
+                    path + "GPO_Terrain_Settings.cfg",
+                    path + "MiniAVC.dll"
                 };
             }
         }
