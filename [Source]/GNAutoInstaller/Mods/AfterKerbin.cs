6a0242c11e31a440497fdc717654609938a27453
Sigma88 <sigmamodexpansions@outlook.com>
2019-08-13 16:09:07 +0200

    afterkerbin fix

--- [Source]/GNAutoInstaller/Mods/AfterKerbin.cs
+++ [Source]/GNAutoInstaller/Mods/AfterKerbin.cs
@@ -6,7 +6,7 @@ namespace GNAutoInstallerPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     class AfterKerbin : Pack<AfterKerbin>
     {
-        internal override string archive { get { return "PluginData/GalacticNeighborhood/After_Kerbin_Planet_Mod-1.1.1.zip"; } }
+        internal override string archive { get { return "PluginData/GalacticNeighborhood/After_Kerbin_Planet_Mod-1.3.0.zip"; } }
         internal override string path { get { return "GameData/AfterKerbin/"; } }
         internal override string[] filter
         {
